01a121a824a968b5ad9005165ecc97529231ee14
steve omari <steveomari62@gmail.com>
2023-05-29 10:23:03 -0400

    update functions, rough draft

--- src/functions _&_tests/functions.js
+++ src/functions _&_tests/functions.js
@@ -1,10 +1,11 @@
 const grabName = (name) => name;
 
 class CreateBoat{
-    constructor(length, numTimesHit=0, hasSunk=false){
+    constructor(length, boatName, numTimesHit=0, hasSunk=false){
         this.length= length;
         this.numTimesHit= numTimesHit;
         this.hasSunk= hasSunk;
+        this.boatName=boatName;
     };
 };
 
@@ -12,4 +13,182 @@ const hit=(boatName) => boatName.numTimesHit++;
 
 const isSunk= (boatName)=> (boatName.length===boatName.numTimesHit) ? true : false;
 
-export { grabName, CreateBoat, hit, isSunk };
+class Gameboard{
+    constructor(){
+        this.coordinates = [
+          //0
+          [
+            { x_number:0, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:0, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:0, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:0, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:0, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:0, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:0, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:0, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:0, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:0, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ],
+          //1
+          [
+            { x_number:1, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:1, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:1, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:1, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:1, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:1, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:1, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:1, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:1, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:1, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ],
+          //2
+          [
+            { x_number:2, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:2, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:2, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:2, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:2, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:2, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:2, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:2, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:2, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:2, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ],
+          //3
+          [
+            { x_number:3, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:3, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:3, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:3, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:3, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:3, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:3, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:3, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:3, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:3, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ],
+          //4
+          [
+            { x_number:4, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:4, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:4, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:4, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:4, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:4, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:4, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:4, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:4, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:4, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ],
+          //5
+          [
+            { x_number:5, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:5, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:5, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:5, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:5, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:5, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:5, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:5, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:5, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:5, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ],
+          //6
+          [
+            { x_number:6, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:6, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:6, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:6, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:6, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:6, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:6, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:6, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:6, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:6, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ],
+          //7
+          [
+            { x_number:7, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:7, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:7, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:7, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:7, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:7, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:7, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:7, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:7, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:7, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ],
+          //8
+          [
+            { x_number:8, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:8, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:8, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:8, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:8, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:8, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:8, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:8, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:8, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:8, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ],
+          //9
+          [
+            { x_number:9, y_number: 0, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:9, y_number: 1, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:9, y_number: 2, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:9, y_number: 3, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:9, y_number: 4, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:9, y_number: 5, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:9, y_number: 6, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:9, y_number: 7, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:9, y_number: 8, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+            { x_number:9, y_number: 9, status: "empty", boatName: "none", boatNameObj: "none", wasPicked: false },
+          ]
+        ];
+    };
+
+    place(shipLength, verticalOrHorizontal, xNumber, yNumber, boatNameObj, boatName){
+      for (let i = 0; i < shipLength; i++) {
+        if(xNumber+i<0 || xNumber+i>9 || yNumber-i<0 || yNumber-i>9){
+          return "Invalid entry, ship is not within the limits of the board.Try again.";
+        };
+        if (verticalOrHorizontal === "horizontal"){
+          this.coordinates[xNumber + i][yNumber].status = "filled";
+          this.coordinates[xNumber + i][yNumber].boatName = boatName;
+          this.coordinates[xNumber + i][yNumber].boatNameObj = boatNameObj;
+        };
+        if (verticalOrHorizontal === "vertical") {
+          this.coordinates[xNumber][yNumber - i].status = "filled";
+          this.coordinates[xNumber][yNumber - i].boatName = boatName;
+          this.coordinates[xNumber][yNumber - i].boatNameObj = boatNameObj;
+        };
+      };
+    };
+
+    receiveAttack(xNumber, yNumber){
+      if (xNumber < 0 || xNumber > 9 || yNumber < 0 || yNumber > 9) {
+        return "Invalid entry, ship is not within the limits of the board.Try again.";
+      }
+      if(this.coordinates[xNumber][yNumber].wasPicked===true){
+        return `Invalid entry, coordinates ${xNumber},${yNumber} was already attacked.Try again.`;
+      };
+      if (this.coordinates[xNumber][yNumber].status === "empty") {
+        //PLACEHOLDER: what to do/display with missed attacks
+        this.coordinates[xNumber][yNumber].wasPicked = true;
+        return `Missed! No hit on coordinates ${xNumber},${yNumber}`;
+      }
+      if (this.coordinates[xNumber][yNumber].status === "filled") {
+        this.coordinates[xNumber][yNumber].wasPicked = true;
+        hit(this.coordinates[xNumber][yNumber].boatNameObj);
+        if (isSunk(this.coordinates[xNumber][yNumber].boatNameObj)===true) {
+          return `Hit! `+(this.coordinates[xNumber][yNumber].boatName)+` has been hit on coordinates ${xNumber},${yNumber}, and has been sunk!`;
+        };
+        return `Hit! `+(this.coordinates[xNumber][yNumber].boatName)+` has been hit on coordinates ${xNumber},${yNumber}`;
+      }
+    };
+};
+
+
+export { grabName, CreateBoat, hit, isSunk, Gameboard };
--- src/functions _&_tests/functions.test.js
+++ src/functions _&_tests/functions.test.js
@@ -1,4 +1,4 @@
-import { grabName, CreateBoat, hit, isSunk } from "./functions";
+import { grabName, CreateBoat, hit, isSunk, Gameboard } from "./functions";
 
 test("grab players name", ()=>{
     expect(grabName("john")).toMatch("john");
@@ -38,4 +38,105 @@ test("boat sinks if damage is high enough", ()=>{
     hit(exampleBoat);
     hit(exampleBoat);
     expect(isSunk(exampleBoat)).toBe(true);
+});
+
+test("get gameboard coordinates & status", () => {
+  const exampleGameboard = new Gameboard();
+
+  expect(exampleGameboard.coordinates[1][3].x_number).toEqual(1);
+  expect(exampleGameboard.coordinates[1][3].y_number).toEqual(3);
+  expect(exampleGameboard.coordinates[1][3].status).toMatch("empty");
+});
+
+test("gameboard can place a 3 square horizontal ship", ()=>{
+  const exampleGameboard = new Gameboard();
+  exampleGameboard.place(3, "horizontal", 2, 2);
+
+  expect(exampleGameboard.coordinates[1][2].status).toMatch("empty");
+  expect(exampleGameboard.coordinates[2][2].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[3][2].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[4][2].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[5][2].status).toMatch("empty");
+});
+
+test("gameboard can place a 3 square vertical ship", ()=>{
+  const exampleGameboard = new Gameboard();
+  exampleGameboard.place(3, "vertical", 3, 6);
+
+  expect(exampleGameboard.coordinates[3][7].status).toMatch("empty");
+  expect(exampleGameboard.coordinates[3][6].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[3][5].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[3][4].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[3][3].status).toMatch("empty");
+});
+
+test("gameboard cannot place ship off the board", ()=>{
+  const exampleGameboard = new Gameboard();
+  
+  expect(exampleGameboard.place(3, "vertical", 1, 1)).toMatch(
+    "Invalid entry, ship is not within the limits of the board.Try again."
+  );
+  expect(exampleGameboard.place(3, "horizontal", 9, 1)).toMatch(
+    "Invalid entry, ship is not within the limits of the board.Try again."
+  );
+  expect(exampleGameboard.place(3, "horizontal", 20, 2)).toMatch(
+    "Invalid entry, ship is not within the limits of the board.Try again."
+  );
+  expect(exampleGameboard.place(3, "horizontal", 2, 20)).toMatch(
+    "Invalid entry, ship is not within the limits of the board.Try again."
+  );
+});
+
+test("use the CreateBoat class constructor to help set boat on gameboard", ()=>{
+  const exampleGameboard = new Gameboard();
+  const exampleBoat = new CreateBoat(4); //length, #times hit, and hasSunk
+  // exampleGameboard.place(3, "horizontal", 2, 2);
+  exampleGameboard.place(exampleBoat.length, "horizontal", 5, 4);
+
+  expect(exampleGameboard.coordinates[4][4].status).toMatch("empty");
+  expect(exampleGameboard.coordinates[5][4].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[6][4].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[7][4].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[8][4].status).toMatch("filled");
+  expect(exampleGameboard.coordinates[9][4].status).toMatch("empty");
+});
+
+test("if an incoming attack hit the players ship", ()=>{
+  const exampleGameboard = new Gameboard();
+  const exampleBoat = new CreateBoat(4, "exampleBoat"); //length, #times hit, and hasSunk
+  // exampleGameboard.place(3, "horizontal", 2, 2);
+
+  exampleGameboard.place(exampleBoat.length, "horizontal", 5, 4, exampleBoat, exampleBoat.boatName);
+
+  expect(exampleGameboard.receiveAttack(2,2)).toMatch("Missed! No hit on coordinates 2,2");
+  expect(exampleGameboard.receiveAttack(3,3)).toMatch("Missed! No hit on coordinates 3,3");
+  expect(exampleGameboard.receiveAttack(5,4)).toMatch("Hit! exampleBoat has been hit on coordinates 5,4");
+  expect(exampleGameboard.receiveAttack(6,4)).toMatch("Hit! exampleBoat has been hit on coordinates 6,4");
+  expect(exampleGameboard.receiveAttack(60,4)).toMatch("Invalid entry, ship is not within the limits of the board.Try again.");
+  expect(exampleBoat.numTimesHit).toBe(2);
+});
+
+test("player receives a sunk message if their boat has been sunk", ()=>{
+  const exampleGameboard = new Gameboard();
+  const exampleBoat = new CreateBoat(4, "exampleBoat"); //length, #times hit, and hasSunk
+
+  exampleGameboard.place(exampleBoat.length, "horizontal", 5, 4, exampleBoat, exampleBoat.boatName);
+
+  expect(exampleGameboard.receiveAttack(5,4)).toMatch("Hit! exampleBoat has been hit on coordinates 5,4");
+  expect(exampleGameboard.receiveAttack(6,4)).toMatch("Hit! exampleBoat has been hit on coordinates 6,4");
+  expect(exampleGameboard.receiveAttack(7,4)).toMatch("Hit! exampleBoat has been hit on coordinates 7,4");
+  expect(exampleBoat.numTimesHit).toBe(3);
+  expect(exampleGameboard.receiveAttack(8,4)).toMatch("Hit! exampleBoat has been hit on coordinates 8,4, and has been sunk!");
+});
+
+test("Player or computer gets an invalid response when they pick an already picked coordinate", ()=>{
+  const exampleGameboard = new Gameboard();
+  const exampleBoat = new CreateBoat(4, "exampleBoat"); //length, #times hit, and hasSunk
+
+  exampleGameboard.place(exampleBoat.length, "horizontal", 5, 4, exampleBoat, exampleBoat.boatName);
+
+  expect(exampleGameboard.receiveAttack(5,4)).toMatch("Hit! exampleBoat has been hit on coordinates 5,4");
+  expect(exampleGameboard.receiveAttack(6,4)).toMatch("Hit! exampleBoat has been hit on coordinates 6,4");
+  expect(exampleGameboard.receiveAttack(5,4)).toMatch("Invalid entry, coordinates 5,4 was already attacked.Try again.");
+  expect(exampleGameboard.receiveAttack(6,4)).toMatch("Invalid entry, coordinates 6,4 was already attacked.Try again.");
 });
